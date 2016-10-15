9745d6aa46089256f5dc1ec861ff5c593615f128
jonabasque <jonmultimedia@gmail.com>
2016-10-15 14:58:27 +0200

    Fixed bug with typeof (typeof == 'string') in Utils.parseNum()

--- spec/Utils/UtilsSpec.js
+++ spec/Utils/UtilsSpec.js
@@ -8,14 +8,19 @@ describe("Utils", function() {
     utilsMockData = {
       num  : "5.2",
       num2 : "10.4",
-      numQuotes : "4,2"
+      numQuotes : "4,2",
+      num3 : true,
+      num4 : {}
     }
-    console.log(typeof utils.parse(utilsMockData.num));
+    //console.log(typeof utils.parseNum(utilsMockData.num));
   });
 
   it("'parse(num)' devuelve lo esperado", function(){
-    expect(utils.parse(utilsMockData.numQuotes)).toEqual(4.2);
-    expect(utils.parse(utilsMockData.num)).toEqual(5.2);
+    expect(utils.parseNum(utilsMockData.num3)).toBeLessThan(1);
+    expect(utils.parseNum(utilsMockData.num3)).toBeGreaterThan(-1);
+    expect(utils.parseNum(utilsMockData.num4)).toEqual(0);
+    expect(utils.parseNum(utilsMockData.numQuotes)).toEqual(4.2);
+    expect(utils.parseNum(utilsMockData.num)).toEqual(5.2);
   });
 
 });
