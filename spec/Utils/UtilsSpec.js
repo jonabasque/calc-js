ac812f3aa53994b9df4446d58fda655d987f45e0
jonabasque <jonmultimedia@gmail.com>
2016-10-15 02:12:57 +0200

    quotes for dotes in parse() method

--- spec/Utils/UtilsSpec.js
+++ spec/Utils/UtilsSpec.js
@@ -7,12 +7,15 @@ describe("Utils", function() {
     utils = new Utils();
     utilsMockData = {
       num  : "5.2",
-      num2 : "10.4"
+      num2 : "10.4",
+      numQuotes : "4,2"
     }
     console.log(typeof utils.parse(utilsMockData.num));
   });
 
   it("'parse(num)' devuelve lo esperado", function(){
+    expect(utils.parse(utilsMockData.numQuotes)).toEqual(4.2);
     expect(utils.parse(utilsMockData.num)).toEqual(5.2);
   });
+
 });
