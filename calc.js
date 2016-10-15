e7ef0780b0daab40d73749341407517ebe0f349c
jonabasque <jonmultimedia@gmail.com>
2016-10-15 22:16:49 +0200

    Added round() util in sum() function

--- calc.js
+++ calc.js
@@ -1,6 +1,30 @@
+var Utils = require("./lib/utils");
+
 module.exports = Calc;
 
 function Calc() {
+  this.utils = new Utils();
     //TODO: poder instanciar el objeto de manera que parsee los operandos.
     //this.parse = true;
+}
+
+Calc.prototype.sum = function(nums, num){
+  console.log(num);
+  //var num2 = num || null;
+	var result = nums[0];
+  console.log(nums);
+  console.log(typeof nums == 'object');
+	if(typeof nums == 'object'){
+    console.log("array!!!");
+    for (var i = 1; i < nums.length; i++) {
+      console.log(nums[i]);
+      var sum2 = this.utils.parseNum(nums[i]);
+      console.log(sum2);
+      result = this.utils.round(result + sum2);
+      console.log(result);
+    }
+	}else{
+		result = this.utils.round(this.utils.parseNum(nums) + this.utils.parseNum(num));
+	}
+  return result;
 }
--- spec/Calc/CalcSpec.js
+++ spec/Calc/CalcSpec.js
@@ -2,17 +2,19 @@ describe("Calc", function() {
   var Calc = require('../../calc');
   console.log(Calc);
   var calc;
+  var nums = [5, "5,2", "4.2", "5.2R", true, {}];
 
   beforeEach(function() {
     calc = new Calc();
     calcMockData = {
-      num  : 5,
-      num2 : 10
+      nums  : [5, "5,2", "4.2", "5.2R", true, {}],
+      num1  : 12,
+      num2  : 10
     }
   });
-/*
-  it("'_parse()' devuelva lo esperado", function(){
-    expect(calc.parse(calcMockData.num, calcMockData.num2)).toEqual()
+
+  it("'plus()' devuelva lo esperado", function(){
+    expect(calc.sum(nums)).toEqual(19.6);
   });
-*/
+
 });
